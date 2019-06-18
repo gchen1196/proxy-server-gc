dd304d819411a040a032e43429c11e948240cab7
George Chen <georgechen@Georges-MacBook-Air-3.local>
2019-06-17 21:43:28 -0700

    refactor proxy to use http proxy

--- server/server.js
+++ server/server.js
@@ -6,35 +6,44 @@ const app = express();
 const port = 3000;
 app.use(express.static(path.join(__dirname, '../public')));
 
+let proxy = require('http-proxy').createProxyServer();
+
+let serverOne = 'http://localhost:3001';
+
 app.get('/restaurants/:id', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
+
 
 app.get('/api/restaurants/:id/photos', (req, res) => {
   const { id } = req.params;
   res.redirect(`http://3.88.8.197:3000/api/restaurants/${id}/photos`);
 })
 
-app.get('/api/restaurants/:id/reviews', (req, res) => {
-  const { id } = req.params;
-  res.redirect(`http://localhost:3001/api/restaurants/${id}/reviews`);
-})
+app.get('/api/restaurants/:id/reviews', (req,res) => proxy.web(req, res, { target: serverOne }));
+// app.get('/api/restaurants/:id/reviews', (req, res) => {
+//   const { id } = req.params;
+//   res.redirect(`http://localhost:3001/api/restaurants/${id}/reviews`);
+// })
 
-app.get('/api/restaurants/:pricesearch&:location', (req, res) => {
-  const { pricesearch } = req.params;
-  const { location } = req.params;
-  res.redirect(`http://localhost:3001/api/restaurants/${pricesearch}&${location}`);
-})
+app.get('/api/restaurants/:pricesearch&:location', (req,res) => proxy.web(req, res, { target: serverOne }));
+// app.get('/api/restaurants/:pricesearch&:location', (req, res) => {
+//   const { pricesearch } = req.params;
+//   const { location } = req.params;
+//   res.redirect(`http://localhost:3001/api/restaurants/${pricesearch}&${location}`);
+// })
 
-app.get('/api/restaurants/:name', (req, res) => {
-  const { name } = req.params;
-  res.redirect(`http://localhost:3001/api/restaurants/${name}`);
-})
+app.get('/api/restaurants/:name', (req,res) => proxy.web(req, res, { target: serverOne }));
+// app.get('/api/restaurants/:name', (req, res) => {
+//   const { name } = req.params;
+//   res.redirect(`http://localhost:3001/api/restaurants/${name}`);
+// })
 
-app.get('/api/restaurants/:location/location', (req, res) => {
-  const { location } = req.params;
-  res.redirect(`http://localhost:3001/api/restaurants/${location}/location`);
-})
+app.get('/api/restaurants/:location/location', (req,res) => proxy.web(req, res, { target: serverOne }));
+// app.get('/api/restaurants/:location/location', (req, res) => {
+//   const { location } = req.params;
+//   res.redirect(`http://localhost:3001/api/restaurants/${location}/location`);
+// })
 
 app.get('/api/restaurants/:id/info', (req, res) => {
   const { id } = req.params;
